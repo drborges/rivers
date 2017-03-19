5fda7ee9f3e0e99d15372ace1df7623af5ac92f0
Diego Borges <drborges.cic@gmail.com>
2017-03-19 20:01:44 -0300

    Singularize aggregator type

--- rivers.go
+++ rivers.go
@@ -24,9 +24,9 @@ type Consumer func(stream.Reader)
 // two new ones.
 type Splitter func(stream.Reader) (stream.Reader, stream.Reader)
 
-// Aggregators implements an aggregation between two streams returning a new
+// Aggregator implements an aggregation between two streams returning a new
 // stream.
-type Aggregators func(stream.Reader, stream.Reader) stream.Reader
+type Aggregator func(stream.Reader, stream.Reader) stream.Reader
 
 // Predicate is a function that given an input it returns true if the input matches
 // the predicate, false otherwise.
